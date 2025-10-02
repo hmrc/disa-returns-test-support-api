931f9d5223d3eb7b7752fa35664b1c098037d669
ttiyemba <57238297+ttiyemba@users.noreply.github.com>
2025-10-02 18:25:46 +0100

    generate report initial work

---
+++ app/uk/gov/hmrc/disareturnstestsupportapi/connectors/CallbackConnector.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.disareturnstestsupportapi.connectors
+
+import play.api.Configuration
+import play.api.libs.json.Json
+import play.api.libs.json.OFormat.oFormatFromReadsAndOWrites
+import play.api.libs.ws.JsonBodyWritables.writeableOf_JsValue
+import uk.gov.hmrc.disareturnstestsupportapi.config.Service
+import uk.gov.hmrc.disareturnstestsupportapi.models.callback.{CallbackRequest, CallbackResponse}
+import uk.gov.hmrc.http.HttpReads.Implicits.readRaw
+import uk.gov.hmrc.http.client.HttpClientV2
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse, StringContextOps}
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class CallbackConnector @Inject() (config: Configuration, httpClient: HttpClientV2)(implicit ec: ExecutionContext) {
+
+  private val baseUrl: String = config.get[Service]("microservice.services.disa-returns")
+
+  def sendMonthlyCallback(zref: String, year: String, month: String, totalRecords: Int)(implicit hc: HeaderCarrier): Future[CallbackResponse] = {
+    val url  = url"$baseUrl/callback/monthly/$zref/$year/$month"
+    val body = CallbackRequest(totalRecords)
+    httpClient
+      .post(url)
+      .withBody(Json.toJson(body))
+      .execute[HttpResponse]
+      .map { response =>
+        response.status match {
+          case 204 => CallbackResponse.Success
+          case _   => CallbackResponse.Failure
+        }
+      }
+  }
+}
---
+++ app/uk/gov/hmrc/disareturnstestsupportapi/controllers/GenerateReportController.scala
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.disareturnstestsupportapi.controllers
+
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.{AbstractController, Action, ControllerComponents}
+import uk.gov.hmrc.disareturnstestsupportapi.connectors.{CallbackConnector, GenerateReportConnector}
+import uk.gov.hmrc.disareturnstestsupportapi.models.GenerateReportRequest
+import uk.gov.hmrc.disareturnstestsupportapi.models.callback.CallbackResponse
+import uk.gov.hmrc.disareturnstestsupportapi.models.common._
+import uk.gov.hmrc.disareturnstestsupportapi.models.errors._
+import uk.gov.hmrc.disareturnstestsupportapi.utils.WithJsonBody
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.http.HeaderCarrierConverter
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class GenerateReportController @Inject()(
+                                          cc: ControllerComponents,
+                                          generateReportConnector: GenerateReportConnector,
+                                          callbackConnector: CallbackConnector
+                                        )(implicit ec: ExecutionContext)
+  extends AbstractController(cc) {
+
+  def generateReport(zRef: String, year: String, month: String): Action[JsValue] =
+    Action.async(parse.json) { implicit request =>
+      implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromRequest(request)
+
+      WithJsonBody[GenerateReportRequest](
+        req =>
+          generateReportConnector
+            .generate(req, zRef, year, month)
+            .flatMap {
+              case GenerateReportResult.Success =>
+                callbackConnector
+                  .sendMonthlyCallback(zRef, year, month, req.totalRecords)
+                  .map {
+                    case CallbackResponse.Success => NoContent
+                    case _       => InternalServerError(Json.toJson(InternalServerErr()))
+                  }
+
+              case _ =>
+                Future.successful(InternalServerError(Json.toJson(InternalServerErr())))
+            }
+            .recover { case _ =>
+              InternalServerError(Json.toJson(InternalServerErr()))
+            },
+        extraValidation = { _ =>
+          val errors: Seq[ErrorResponse] = List(
+            Option.unless(IsaRefValidator.isValid(zRef))(InvalidZref),
+            Option.unless(TaxYearValidator.isValid(year))(InvalidTaxYear),
+            Option.unless(MonthValidator.isValid(month))(InvalidMonth)
+          ).flatten
+
+          if (errors.nonEmpty) Some(ValidationFailureResponse.createFromErrorResponses(errors))
+          else None
+        }
+      )
+    }
+
+
+}
