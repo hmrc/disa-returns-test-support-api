a08d66b6a9488d71ffec429e13de9e1e364fede9
zach.hall <29452017+zachhall1234@users.noreply.github.com>
2026-02-11 18:34:13 +0000

    DFI-1622 report gen logic moved to service layer

--- app/uk/gov/hmrc/disareturnstestsupportapi/controllers/GenerateReportController.scala
+++ app/uk/gov/hmrc/disareturnstestsupportapi/controllers/GenerateReportController.scala
@@ -19,12 +19,11 @@ package uk.gov.hmrc.disareturnstestsupportapi.controllers
 import play.api.Logging
 import play.api.libs.json.Json
 import play.api.mvc._
-import uk.gov.hmrc.disareturnstestsupportapi.connectors.{DisaReturnsCallbackConnector, GenerateReportConnector}
 import uk.gov.hmrc.disareturnstestsupportapi.controllers.actions.AuthAction
 import uk.gov.hmrc.disareturnstestsupportapi.models.GenerateReportRequest
-import uk.gov.hmrc.disareturnstestsupportapi.models.callback.CallbackResponse
 import uk.gov.hmrc.disareturnstestsupportapi.models.common._
 import uk.gov.hmrc.disareturnstestsupportapi.models.errors._
+import uk.gov.hmrc.disareturnstestsupportapi.service.GenerateReportService
 import uk.gov.hmrc.disareturnstestsupportapi.utils.RequestParser
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.http.HeaderCarrierConverter
@@ -33,63 +32,50 @@ import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
 class GenerateReportController @Inject() (
-  cc:                      ControllerComponents,
-  generateReportConnector: GenerateReportConnector,
-  callbackConnector:       DisaReturnsCallbackConnector,
-  authAction:              AuthAction,
-  requestParser:           RequestParser
-)(implicit ec:             ExecutionContext)
+  cc:                    ControllerComponents,
+  authAction:            AuthAction,
+  requestParser:         RequestParser,
+  generateReportService: GenerateReportService
+)(implicit ec:           ExecutionContext)
     extends AbstractController(cc)
     with Logging {
 
   def generateReport(zRef: String, year: String, month: String): Action[AnyContent] =
-    Action.async { implicit request: Request[AnyContent] =>
-      implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromRequest(request)
+    Action.async { implicit request =>
+      implicit val hc: HeaderCarrier =
+        HeaderCarrierConverter.fromRequest(request)
       requestParser.parseJson[GenerateReportRequest](request) match {
         case Left(errorResult) =>
           Future.successful(errorResult)
+
         case Right(req) =>
           validateParams(zRef, year, month) match {
             case Left(errorResult) =>
               Future.successful(errorResult)
 
-            case Right((validZRef, validYear, validMonth)) =>
-              authAction(validZRef).invokeBlock(
-                request,
-                { _: Request[AnyContent] =>
-                  generateReportConnector
-                    .generateReport(req, validZRef, validYear, validMonth)
-                    .flatMap {
-                      case GenerateReportResult.Success =>
-                        callbackConnector
-                          .callback(validZRef, validYear, validMonth, req.totalRecords)
-                          .map {
-                            case CallbackResponse.Success =>
-                              logger.info(
-                                s"[GenerateReportController] Generate Report successful zRef=$validZRef, year=$validYear, month=$validMonth."
-                              )
-                              NoContent
-                            case _ =>
-                              logger.error(
-                                s"[GenerateReportController] Callback failed zRef=$validZRef, year=$validYear, month=$validMonth"
-                              )
-                              InternalServerError(Json.toJson(InternalServerErr()))
-                          }
+            case Right((validZRef, y, m)) =>
+              authAction(validZRef)
+                .invokeBlock(
+                  request,
+                  { _: Request[AnyContent] =>
+                    generateReportService
+                      .generateReport(req, validZRef, y, m)
+                      .map {
+                        case GenerateReportResult.Success =>
+                          NoContent
 
-                      case GenerateReportResult.Failure =>
-                        logger.error(
-                          s"[GenerateReportController] Generate Report failed zRef=$validZRef, year=$validYear, month=$validMonth"
-                        )
-                        Future.successful(InternalServerError(Json.toJson(InternalServerErr())))
-                    }
-                    .recover { case _ =>
-                      logger.error(
-                        s"[GenerateReportController] Unexpected error during Generate Report zRef=$validZRef, year=$validYear, month=$validMonth"
-                      )
-                      InternalServerError(Json.toJson(InternalServerErr()))
-                    }
+                        case GenerateReportResult.Failure =>
+                          InternalServerError(Json.toJson(InternalServerErr()))
+                      }
+                  }
+                )
+                .recover { case e =>
+                  logger.error(
+                    s"[GenerateReportController] Unexpected error zRef=$zRef year=$year month=$month",
+                    e
+                  )
+                  InternalServerError(Json.toJson(InternalServerErr()))
                 }
-              )
           }
       }
     }
